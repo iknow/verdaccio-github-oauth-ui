6dee5bdd5e3366d5dd7f169831b87761034fac60
Thomas Dy <thatsmydoing@gmail.com>
2022-02-21 17:29:01 +0900

    Hack fix for verdaccio 5.5

--- src/client/verdaccio-5.ts
+++ src/client/verdaccio-5.ts
@@ -2,7 +2,7 @@ import { getUsageInfo, init, isLoggedIn } from "./plugin"
 
 const helpCardUsageInfoSelector = "#help-card .MuiCardContent-root span"
 const dialogUsageInfoSelector =
-  "#registryInfo--dialog-container .MuiDialogContent-root .MuiTypography-root span"
+  "#registryInfo--dialog-container .MuiDialogContent-root .MuiCollapse-root .MuiBox-root > div > span"
 const randomClass = "Os1waV6BSoZQKfFwNlIwS"
 
 async function copyToClipboard(text: string) {
